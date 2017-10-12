64b02596deafad0f5b9250eacee79a631f488ab5
Luciano Rossi <lukio@gcoop.coop>
2017-10-12 11:55:10 -0300

    fix #2 validated invoices must get amount to pay

--- invoice.py
+++ invoice.py
@@ -5,6 +5,8 @@ import logging
 
 from trytond.model import Workflow, ModelView
 from trytond.pool import Pool, PoolMeta
+from trytond.transaction import Transaction
+from trytond.modules.account_invoice.invoice import _ZERO
 
 logger = logging.getLogger(__name__)
 
@@ -14,6 +16,47 @@ __all__ = ['Invoice']
 class Invoice:
     __name__ = 'account.invoice'
     __metaclass__ = PoolMeta
+
+    @classmethod
+    def get_amount_to_pay(cls, invoices, name):
+        pool = Pool()
+        Currency = pool.get('currency.currency')
+        Date = pool.get('ir.date')
+
+        today = Date.today()
+        res = dict((x.id, _ZERO) for x in invoices)
+        for invoice in invoices:
+            if invoice.state in ['draft', 'cancel', 'paid']:
+                continue
+            amount = _ZERO
+            amount_currency = _ZERO
+            for line in invoice.lines_to_pay:
+                if line.reconciliation:
+                    continue
+                if (name == 'amount_to_pay_today'
+                        and line.maturity_date > today):
+                    continue
+                if (line.second_currency
+                        and line.second_currency == invoice.currency):
+                    amount_currency += line.amount_second_currency
+                else:
+                    amount += line.debit - line.credit
+            for line in invoice.payment_lines:
+                if line.reconciliation:
+                    continue
+                if (line.second_currency
+                        and line.second_currency == invoice.currency):
+                    amount_currency += line.amount_second_currency
+                else:
+                    amount += line.debit - line.credit
+            if amount != _ZERO:
+                with Transaction().set_context(date=invoice.currency_date):
+                    amount_currency += Currency.compute(
+                        invoice.company.currency, amount, invoice.currency)
+            if invoice.type == 'in':
+                amount_currency *= -1
+            res[invoice.id] = amount_currency
+        return res
 
     @classmethod
     @ModelView.button
